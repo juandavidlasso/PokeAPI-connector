58994d087d220536e4e00f608dee6a99e39e3cdf
Juan David Lasso <juandavid.lasso@dialapplet.com>
2024-09-18 15:29:06 -0500

    Prueba sendifico pokeAPI connector

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { PokemonController } from './pokemon/pokemon.controller';
 import { PokemonService } from './pokemon/pokemon.service';
 
 @Module({
-    imports: [PokemonModule, HttpModule],
+    imports: [ConfigModule.forRoot(), PokemonModule, HttpModule],
     controllers: [PokemonController],
     providers: [PokemonService],
 })
